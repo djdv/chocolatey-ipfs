94a3a5b57f7e7165c443e12292feb5091d5f4a4e
Dominic Della Valle <ddvpublic@gmail.com>
2020-04-28 13:00:49 -0400

    Update license, description, tags, and icon

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'ipfs'
-$url32 = 'https://dist.ipfs.io/go-ipfs/v0.4.19/go-ipfs_v0.4.19_windows-386.zip'
-$checksum32 = 'bcd6a50f9bebb8aacb92fd5a2f30b7c534742283fe9357cc9b253a83e4b42645a855a2e302556ff644f9c17dfb30301abe70cdb438d2fc06b51511077155582a'
+$url32 = 'https://dist.ipfs.io/go-ipfs/v0.5.0/go-ipfs_v0.5.0_windows-386.zip'
+$checksum32 = '3563da55343cb9c456df54d88cacb7a6cf981715d787b85db01afc5e0f52c2405fea3ca631e938242a2857e58f3cda2f16919fb331efce0d8b3a4f5adf28a10d'
 $checksumType = 'sha512'
-$url64 = 'https://dist.ipfs.io/go-ipfs/v0.4.19/go-ipfs_v0.4.19_windows-amd64.zip'
-$checksum64 = 'e3aca9276dc0b71e351447f1c129cd26b76a7a13334fa47ba84966efdc9b2bb75d154e7df31a57f8ddad9f548e49373340efe93ad0123b2ae3ae92b64193b91a'
+$url64 = 'https://dist.ipfs.io/go-ipfs/v0.5.0/go-ipfs_v0.5.0_windows-amd64.zip'
+$checksum64 = 'c8a6de566002c947f3483e6af251682f2c6d225305d872ac89a37c124130d2f52270b4c5a31f017d788dc96c02f6121a6a2ec71ef655228ffec340712e5d17c7'
 $checksumType64 = 'sha512'
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
